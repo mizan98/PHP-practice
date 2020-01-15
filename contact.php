33e08719a56915b1d1d7d83719739869b53f1e7a
Mizan Choudhury <mizan_98@hotmail.co.uk>
2020-01-15 12:15:58 +0000

    inserting header in contact page

--- contact.php
+++ contact.php
@@ -1,21 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <ul>
-        <li><a href="home.php">Home</a></li>
-        <li><a href="about.php">About</a></li>
-        <li><a href="contact.php">Contact</a></li>
-    </ul>
-    <h1>This is the contact page</h1>
+<?php 
+    $page = "Contact";
 
-    <footer>
-        <p>&copy;<?php echo date ('Y') ?> All Rights Reserved</p>
-    </footer>
-</body>
-</html>
+?>
+
+<?php include './header.php'?>
+    <h1>This is the Contact page</h1>
--- inc/footer.php
+++ inc/footer.php
@@ -1,5 +1,6 @@
 <footer>
         <p>&copy;<?php echo date ('Y') ?> All Rights Reserved</p>
+        <p>MAde with &hearts; Manchester, Uk</p>
     </footer>
 </body>
 </html>
